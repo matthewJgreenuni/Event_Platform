1f08c8454da57afb528c7ab7a7508681bc9b5c8e
Jack Sharkey <113288400+Sharkey0110@users.noreply.github.com>
2024-02-03 19:05:09 +0000

    Event render

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,13 @@ import { connectToDB } from "../database";
 import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+async function populateEvent(query: any) {
+    return query
+    .populate({ path: 'organizer', model: User, select: '_id firstName lastName'})
+    .populate({ path: 'category', model: Category, select: '_id name'})
+}
 
 export async function createEvent({ event, userId, path}: CreateEventParams) {
     try{
@@ -20,3 +27,18 @@ export async function createEvent({ event, userId, path}: CreateEventParams) {
         handleError(error)
     }
 }
+
+export async function getEventById(eventId: string) {
+    try{
+        connectToDB();
+        const event = await populateEvent(Event.findById(eventId))
+        if (!event){
+            throw new Error ("Event not found")
+        } 
+        return JSON.parse(JSON.stringify(event))
+
+    }catch(error){
+        handleError(error)
+    }
+}
+
